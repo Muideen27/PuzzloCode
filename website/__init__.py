a8310e9d07377dba835f88a7dc68bd8923a8aed0
Muideen27 <ilorimuideen0000@gmail.com>
2023-10-09 17:47:15 +0100

    using postgreql services of render deployment

--- website/__init__.py
+++ website/__init__.py
@@ -19,11 +19,6 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
-    
-    with app.app_context():
-        db.create_all()
-
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
