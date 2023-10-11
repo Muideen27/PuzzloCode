06f1979310c9b95e02b2011f4920fd2ca21c160c
Muideen27 <ilorimuideen0000@gmail.com>
2023-10-11 18:48:33 +0100

    Files updated

--- website/models.py
+++ website/models.py
@@ -7,15 +7,26 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
 
-class Question(db.Model):
+class CodingQuestion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255), nullable=False)
-    difficulty = db.Column(db.String(50), nullable=False)
-    topic = db.Column(db.String(100))
     question_text = db.Column(db.Text, nullable=False)
+    difficulty = db.Column(db.String(50), nullable=False)
+    topic = db.Column(db.String(100), nullable=False)
 
-    def __init__(self, title, difficulty, topic, question_text):
-        self.title = title
-        self.difficulty = difficulty
-        self.topic = topic
-        self.question_text = question_text
+    # Fields for multiple-choice options
+    option_A = db.Column(db.String(250), nullable=True)
+    option_B = db.Column(db.String(250), nullable=True)
+    option_C = db.Column(db.String(250), nullable=True)
+    option_D = db.Column(db.String(250), nullable=True)
+
+    # Correct answer (e.g., 'A', 'B', 'C', or 'D')
+    correct_answer = db.Column(db.String(1), nullable=False)
+
+    # Additional fields you might consider:
+    # - Explanation for the correct answer
+    # - Tags or categories for better organization
+    # - Creation date or timestamp
+    # - Author or source of the question
+
+    def __repr__(self):
+        return f'<CodingQuestion: {self.id}>'
