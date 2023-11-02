dbfe974b6cc4022217cb8db531148c82aca10242
Muideen27 <ilorimuideen0000@gmail.com>
2023-11-02 12:51:06 +0000

    updating packages to suite render installation

--- website/assessment.py
+++ website/assessment.py
@@ -5,11 +5,11 @@ from flask_login import login_required, current_user
 
 assessment = Blueprint('assessment', __name__)
 
-@assessment.route('/api/questions', methods=['GET'])  # Adjust the route to /api/questions
+@assessment.route('/api/questions', methods=['GET'])
 @login_required
 def get_assessment_questions():
     if current_user.is_authenticated:
-        question_count = int(request.args.get('count', 1))  # Get the number of questions requested
+        question_count = int(request.args.get('count', 1))
         questions = HTMLAssessment.query.order_by(func.random()).limit(question_count).all()
 
         questions_json = [question.to_dict() for question in questions]
